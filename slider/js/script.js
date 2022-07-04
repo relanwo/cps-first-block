7b968c9bf7e0a132e3e86a75cd7ab94296ef63df
Anna Tostanovskaya <relanwo@gmail.com>
2022-07-04 17:10:23 +0300

    блок 1.5

---
+++ slider/js/script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const width = window.innerWidth
+  if (width < 767){
+    const slider = new Swiper()
+  }
+})
+
+// initializing swiper
+new Swiper('.swiper-container', {
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+});
+
+// раскрытие/закрытие slider при разных разрешениях
+var button = document.querySelector(".slider__dropdown");
+var cards = document.getElementsByClassName("slider__card");
+
+var changeButton = function () {
+  if (button.innerHTML === "Показать все") {
+    button.innerHTML = "Скрыть";
+  } else {
+    button.innerHTML = "Показать все";
+  }
+
+  button.classList.toggle('slider__dropdown--active');
+};
+
+let arr = [];
+let param;
+let selector; 
+  
+let resize = function () {
+  param = 6;
+  selector = 'slider__card--hidden768'; 
+
+  if (window.matchMedia('(min-width: 1120px)').matches) {
+    selector = 'slider__card--hidden1120';
+    param = 8;
+  }
+
+  arr.push(param, selector)
+  return arr;
+};
+
+button.addEventListener('click', function () {
+  resize();
+  for (var i = arr[0]; i < cards.length; i++) {
+    cards[i].classList.toggle(arr[1]);
+  }
+  arr.length = 0;
+  changeButton(button.innerHTML);
+});
+
+// window.addEventListener('resize', function () {
+//   if (document.documentElement.clientWidth = 1120) {
+//     button.innerHTML = "Показать все"
+//   } else if (document.documentElement.clientWidth = 768) {
+//     button.innerHTML = "Показать все"
+//   }
+// });
